ba3ec238e70355730cc2459658a0baa465df43f7
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-06-30 14:28:48 -0400

    Path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+import packageJSON from './package.json';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: { mode: string }) => {
+    const isDev = mode === 'development';
+
+    // GH-pages has /<repo>/ in the path (see "homepage" in package.json)
+    const splitHomepage = packageJSON.homepage.split('/');
+    const base = !isDev ? `/${splitHomepage[splitHomepage.length - 2]}/` : '/';
+
+    return defineConfig({
+        base,
+        plugins: [
+            react(),
+            checker({
+                typescript: isDev,
+                overlay: {
+                    initialIsOpen: false,
+                },
+            }),
+        ],
+    });
+};
