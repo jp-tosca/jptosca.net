fae5ae7c2b50429459ddbc8ae2365ddfcd8f0b7e
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-07-12 15:38:17 -0400

    refactor: remove commented-out code from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,3 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
 })
-
-// export default ({ mode }: { mode: string }) => {
-//     const isDev = mode === 'development';
-
-//     // GH-pages has /<repo>/ in the path (see "homepage" in package.json)
-//     const splitHomepage = packageJSON.homepage.split('/');
-//     const base = !isDev ? `/${splitHomepage[splitHomepage.length - 2]}/` : '/';
-
-//     return defineConfig({
-//         base,
-//         plugins: [
-//             react(),
-//             checker({
-//                 typescript: isDev,
-//                 overlay: {
-//                     initialIsOpen: false,
-//                 },
-//             }),
-//         ],
-//     });
-// };
