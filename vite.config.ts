0322ef5b2c7b6f8074c7f06741f5285df3b51250
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-07-12 15:26:40 -0400

    refactor: simplify Vite configuration by removing unused code and comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import checker from 'vite-plugin-checker';
-import packageJSON from './package.json';
 
-export default ({ mode }: { mode: string }) => {
-    const isDev = mode === 'development';
+export default defineConfig({
+  plugins: [react()],
+})
 
-    // GH-pages has /<repo>/ in the path (see "homepage" in package.json)
-    const splitHomepage = packageJSON.homepage.split('/');
-    const base = !isDev ? `/${splitHomepage[splitHomepage.length - 2]}/` : '/';
+// export default ({ mode }: { mode: string }) => {
+//     const isDev = mode === 'development';
 
-    return defineConfig({
-        base,
-        plugins: [
-            react(),
-            checker({
-                typescript: isDev,
-                overlay: {
-                    initialIsOpen: false,
-                },
-            }),
-        ],
-    });
-};
+//     // GH-pages has /<repo>/ in the path (see "homepage" in package.json)
+//     const splitHomepage = packageJSON.homepage.split('/');
+//     const base = !isDev ? `/${splitHomepage[splitHomepage.length - 2]}/` : '/';
+
+//     return defineConfig({
+//         base,
+//         plugins: [
+//             react(),
+//             checker({
+//                 typescript: isDev,
+//                 overlay: {
+//                     initialIsOpen: false,
+//                 },
+//             }),
+//         ],
+//     });
+// };
